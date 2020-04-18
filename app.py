90b16a2f6e08a59c1c424fa2383025ef1aba9861
Your Name <you@example.com>
2020-04-18 00:31:50 -0300

    initial commit

---
+++ app.py
@@ -0,0 +1,36 @@
+from unicorn_binance_websocket_api.unicorn_binance_websocket_api_manager import BinanceWebSocketApiManager
+from unicorn_fy.unicorn_fy import UnicornFy
+from time import sleep
+binance_websocket_api_manager = BinanceWebSocketApiManager(exchange="binance.com")
+
+markets = ['paxtusd', 'usdcpax','usdctusd','paxusdt','busdusdt','tusdusdt','usdcusdt','usdsusdt']
+channels = ['ticker']
+
+fee = 0.0240 * 2
+fee = fee * 0.6
+print(fee)
+binance_websocket_api_manager.create_stream(channels, markets)
+bids = {}
+bidAmts = {}
+asks = {}
+askAmts = {}
+while True:
+	oldest_stream_data_from_stream_buffer = binance_websocket_api_manager.pop_stream_data_from_stream_buffer()
+	if oldest_stream_data_from_stream_buffer:
+		unicorn_fied_stream_data = UnicornFy.binance_com_websocket(oldest_stream_data_from_stream_buffer)
+		
+		if 'data' in unicorn_fied_stream_data:
+			for d in unicorn_fied_stream_data['data']:
+				bids[d['symbol']] = (d['best_bid_price'])
+				asks[d['symbol']] = (d['best_ask_price'])
+				bidAmts[d['symbol']] = (d['best_bid_quantity'])
+				askAmts[d['symbol']] = (d['best_ask_quantity'])
+	for order in bids:
+		arb = (float(asks[order]) / float(bids[order]) - 1) * 100#/  1.1 / 0.9
+		
+		if arb > fee:
+			print(order + ': ' + str(arb))
+			if float(bidAmts[order]) < float(askAmts[order]):	
+				print(str(bidAmts[order]) + ' available at this price!')
+			else:
+				print(str(askAmts[order]) + ' available at this price!')
