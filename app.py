101d2c7519cc9fe8e3fa5d8f4f310ba5a6004f97
Your Name <you@example.com>
2020-04-20 23:21:17 -0300

    testing model

--- app.py
+++ app.py
@@ -6,9 +6,18 @@ binance_websocket_api_manager = BinanceWebSocketApiManager(exchange="binance.com
 markets = ['paxtusd', 'usdcpax','usdctusd','paxusdt','busdusdt','tusdusdt','usdcusdt','usdsusdt']
 channels = ['ticker']
 
-fee = 0.0240 * 2
-fee = fee * 0.6
-print(fee)
+tiers = {3: 0.06 * 0.6, 4: 0.054, 5: 0.048, 6: 0.042, 7:0.036, 8: 0.030, 9: 0.024}
+
+
+winnersCounts = {}
+winnersArbs = {}
+winnersVols = {}
+winnersArbsAvg = {}
+for fee in tiers:
+	tiers[fee] = tiers[fee] * 2
+	winnersCounts[fee] = 0
+	winnersArbs[fee] = []
+	winnersVols[fee] = 0
 binance_websocket_api_manager.create_stream(channels, markets)
 bids = {}
 bidAmts = {}
@@ -28,12 +37,27 @@ while True:
 				askAmts[d['symbol']] = (d['best_ask_quantity'])
 	for order in bids:
 		arb = (float(asks[order]) / float(bids[order]) - 1) * 100#/  1.1 / 0.9
-		
-		if arb > fee:
-			if {'askAmts': askAmts[order], 'bidAmts': bidAmts[order]} not in alreadyDone:
-				alreadyDone.append({'askAmts': askAmts[order], 'bidAmts': bidAmts[order]})
-				print(order + ': ' + str(arb))
-				if float(bidAmts[order]) < float(askAmts[order]):	
-					print(str(bidAmts[order]) + ' available at this price!')
-				else:
-					print(str(askAmts[order]) + ' available at this price!')
+		for tier in tiers:
+			fee = tiers[tier]
+			if arb > fee:
+				if {fee: {'askAmts': askAmts[order], 'bidAmts': bidAmts[order]}} not in alreadyDone:
+					alreadyDone.append({fee: {'askAmts': askAmts[order], 'bidAmts': bidAmts[order]}})
+
+					winnersCounts[tier] = winnersCounts[tier] + 1
+					winnersArbs[tier].append(arb)
+					t = 0
+					for winarb in winnersArbs[tier]:
+						t = t + winarb
+					winnersArbsAvg[tier] = t / winnersCounts[tier]
+					
+					print(order + ': ' + str(arb))
+					if float(bidAmts[order]) < float(askAmts[order]):	
+						winnersVols[tier] = winnersVols[tier]  + float(bidAmts[order])
+					else:	
+						winnersVols[tier] = winnersVols[tier]  + float(askAmts[order])
+					print('winnerCounts')
+					print(winnersCounts)
+					print('winnerArbsAvg')
+					print(winnersArbsAvg)
+					print('winnerVols')
+					print(winnersVols)
